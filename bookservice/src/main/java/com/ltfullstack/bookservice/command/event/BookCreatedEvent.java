07898ca0c830adca685fb0df0f970650f99e6488
Pham Minh <phamminhpro1405@gmail.com>
2026-02-16 15:07:54 +0700

    feat(BE): Write API Create a Book

--- bookservice/src/main/java/com/ltfullstack/bookservice/command/event/BookCreatedEvent.java
+++ bookservice/src/main/java/com/ltfullstack/bookservice/command/event/BookCreatedEvent.java
@@ -1,21 +1,15 @@
-package com.ltfullstack.bookservice.command.data;
+package com.ltfullstack.bookservice.command.event;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
-@Table(name = "books")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Book {
-    @Id
+public class BookCreatedEvent {
     private String id;
 
     private String name;
