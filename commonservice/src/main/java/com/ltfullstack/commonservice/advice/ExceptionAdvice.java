6e3ded9f24a8667622092d17bd9aeda4b89445c5
Pham Minh <phamminhpro1405@gmail.com>
2026-02-17 15:37:30 +0700

    feat(BE): Exception Handling

---
+++ commonservice/src/main/java/com/ltfullstack/commonservice/advice/ExceptionAdvice.java
@@ -0,0 +1,17 @@
+package com.ltfullstack.commonservice.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.ltfullstack.commonservice.model.ErrorMessage;
+
+@ControllerAdvice
+public class ExceptionAdvice {
+    
+    @ExceptionHandler
+    public ResponseEntity<ErrorMessage> handleException(Exception ex) {
+        return new ResponseEntity(new ErrorMessage("9999", ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
