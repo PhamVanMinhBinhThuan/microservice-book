40460fc2301fff0f872772582dcfcd1ed0621c7f
Pham Minh <phamminhpro1405@gmail.com>
2026-02-17 23:51:56 +0700

    feat(BE): Handling Disrupted Events

--- employeeservice/src/main/java/com/ltfullstack/employeeservice/command/event/EmployeeEventHandler.java
+++ employeeservice/src/main/java/com/ltfullstack/employeeservice/command/event/EmployeeEventHandler.java
@@ -2,6 +2,7 @@ package com.ltfullstack.employeeservice.command.event;
 
 import java.util.Optional;
 
+import org.axonframework.eventhandling.DisallowReplay;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +12,12 @@ import com.ltfullstack.employeeservice.command.data.Employee;
 import com.ltfullstack.employeeservice.command.data.EmployeeRepository;
 
 import jakarta.ws.rs.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 public class EmployeeEventHandler {
-    
+
     @Autowired
     private EmployeeRepository employeeRepository;
 
@@ -28,19 +31,24 @@ public class EmployeeEventHandler {
     @EventHandler
     private void on(EmployeeUpdatedEvent event) {
         Optional<Employee> oldEmployee = employeeRepository.findById(event.getId());
-        
+
         Employee employee = oldEmployee.orElseThrow(() -> new NotFoundException("Employee not found"));
         employee.setFirstName(event.getFirstName());
         employee.setLastName(event.getLastName());
         employee.setKin(event.getKin());
         employee.setIsDisciplined(event.getIsDisciplined());
-        
+
         employeeRepository.save(employee);
     }
 
     @EventHandler
+    @DisallowReplay
     private void on(EmployeeDeletedEvent event) throws Exception {
-        employeeRepository.findById(event.getId()).orElseThrow(() -> new Exception("Employee not found"));
-        employeeRepository.deleteById(event.getId());
+        try {
+            employeeRepository.findById(event.getId()).orElseThrow(() -> new Exception("Employee not found"));
+            employeeRepository.deleteById(event.getId());
+        } catch (Exception ex) {
+            log.error(ex.getMessage());
+        }
     }
 }
