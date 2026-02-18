ad09a8d3792b59866397272a3a6544c18b2827e5
Pham Minh <phamminhpro1405@gmail.com>
2026-02-18 13:01:31 +0700

    feat(BE): Write APIs Get ALL & Get Detail

--- employeeservice/src/main/java/com/ltfullstack/employeeservice/command/data/EmployeeRepository.java
+++ employeeservice/src/main/java/com/ltfullstack/employeeservice/command/data/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package com.ltfullstack.employeeservice.command.data;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, String> {
-    
+    List<Employee> findAllByIsDisciplined(Boolean isDisciplined);
 }
